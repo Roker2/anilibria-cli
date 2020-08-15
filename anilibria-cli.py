508d99553bec5df3f4ee61785b54bc1ef4c7336c
CakesTwix <oleg.kiryazov@gmail.com>
2020-08-15 14:56:37 +0300

    anilibria-cli: Обновление find -> findtitle * Добавил флаг описания

--- anilibria-cli.py
+++ anilibria-cli.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import click
 import requests
 
@@ -9,22 +10,24 @@ def cli():
 .%%%%%%..%%.%%%....%%....%%........%%....%%%%%...%%%%%.....%%....%%%%%%.
 .%%..%%..%%..%%....%%....%%........%%....%%..%%..%%..%%....%%....%%..%%.
 .%%..%%..%%..%%..%%%%%%..%%%%%%..%%%%%%..%%%%%...%%..%%..%%%%%%..%%..%%.
-........................................................................"""
-
-
+..................AniLibria-cli от Тортика..............................
+........................................................................
+"""
 
 
 @click.command()
 @click.option('--title', help='Информация об тайтле')
-def find(title):
-    response = requests.get('https://api.anilibria.tv/v2/searchTitles?search='+title)
+@click.option('--desc', is_flag=True, help='Вывести описание тайтла')
+def findtitle(title, desc):
+    response = requests.get('https://api.anilibria.tv/v2/searchTitles?search=' + title)
     name = response.json()
     for item in name:
-        click.echo(item['names']['ru'])
+        click.echo(item['names']['ru'] + ' | ' + item['names']['en'])
+        if desc:
+            click.echo(item['description'])
 
 
-cli.add_command(find)
-
+cli.add_command(findtitle)
 
 if __name__ == '__main__':
     cli()
